86492b6a1bd806f7de659f0cbac6f40c4e616b82
Mark te Poele <mark.tepoele@itvitaelearning.nl>
2021-08-12 10:03:37 +0200

    Add project files.

---
+++ TDDExercise/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TDDExercise
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+    }
+}
